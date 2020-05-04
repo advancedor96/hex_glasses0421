e341742c6141d83b4d8e221a5d4c6164154406d9
丁丁 <advancedor96@gmail.com>
2020-05-04 17:27:12 +0800

    移到 compass 版

---
+++ js/index.js
@@ -0,0 +1,12 @@
+$('#desktop').click(() => {
+  console.log('click')
+  window.location.pathname= '/desktop'
+});
+$('#tablet').click(() => {
+  console.log('click')
+  window.location.pathname= '/tablet'
+});
+$('#mobile').click(() => {
+  console.log('click')
+  window.location.pathname= '/mobile'
+});
