23ef70168f46b6ca889c8712488dd9ad5490044d
丁丁 <advancedor96@gmail.com>
2020-05-06 21:22:08 +0800

    Updates

--- js/index.js
+++ js/index.js
@@ -1,13 +1,13 @@
 $('#desktop').click(() => {
   // window.location.pathname= '/pages/desktop'
-  let win = window.open('/pages/desktop', '_blank');
+  let win = window.open('./pages/desktop', '_blank');
   win.focus();
 });
 $('#tablet').click(() => {
-  let win = window.open('/pages/tablet', '_blank');
+  let win = window.open('./pages/tablet', '_blank');
   win.focus();
 });
 $('#mobile').click(() => {
-  let win = window.open('/pages/mobile', '_blank');
+  let win = window.open('./pages/mobile', '_blank');
   win.focus();
 });
